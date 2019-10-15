f9731c0df19114251a61d8ed6d867dc80e62d7bb
fcxoliveira <filcarx@gmail.com>
2019-10-15 20:36:13 -0300

    Alterando função para gráfico de média de gols

--- generate_graph_brasileirao.py
+++ generate_graph_brasileirao.py
@@ -4,13 +4,13 @@ import pandas as pd
 from matplotlib.ticker import MaxNLocator
 from mpl_toolkits.axes_grid1 import host_subplot
 
-path_base = "C:/Users/filip/PycharmProjects/learn-pandas/Dataset Brasileirão/"
+path_base = "C:/Users/filip/PycharmProjects/learn-pandas/"
+campeonato = "2019-Match-SerieA"
 
-
+#Se basear por este gráfico para a geração dos demais
 def CalcularMediaGolsPorAno(ano):
-    table = pd.read_csv(path_base + str(ano) + "_partidas.csv", encoding="UTF-8", sep=',')
-    table["Saldo"] = table["placar_oficial_mandante"] + table["placar_oficial_visitante"]
-    average = table["Saldo"].sum()/table.shape[0]
+    data = pd.read_csv(path_base + str(ano) + campeonato + ".csv", encoding="UTF-8", sep='\t')
+    average = data.groupby('MatchId')['score'].sum().mean()
     return average
 
 
@@ -36,22 +36,23 @@ def CalcularPartidasComGol(ano):
     result = table[table["qtgols"] > 0].qtgols.count()
     return result
 #years = [2014, 2015, 2016, 2017]
-years = [2014, 2015, 2016]
+years = [""]
 
 medias_gols_por_ano = list(map(CalcularMediaGolsPorAno, years))
-medias_gols_mandante_por_ano = list(map(CalcularMediaGolsPorAnoMandante, years))
-medias_gols_visitante_por_ano = list(map(CalcularMediaGolsPorAnoVisitante, years))
-qt_partidas_sem_gol = list(map(CalcularPartidasSemGol, years))
-qt_partidas_com_gol = list(map(CalcularPartidasComGol, years))
+print(medias_gols_por_ano)
+#medias_gols_mandante_por_ano = list(map(CalcularMediaGolsPorAnoMandante, years))
+#medias_gols_visitante_por_ano = list(map(CalcularMediaGolsPorAnoVisitante, years))
+#qt_partidas_sem_gol = list(map(CalcularPartidasSemGol, years))
+#qt_partidas_com_gol = list(map(CalcularPartidasComGol, years))
 
 #Gráfico 1 - Média de gols por jogo anual
-data = {'Ano': years, 'Média': medias_gols_por_ano}
-tabela_final = pd.DataFrame(data=data)
+#data = {'Ano': years, 'Média': medias_gols_por_ano}
+#tabela_final = pd.DataFrame(data=data)
 
-print(tabela_final.head(4))
-ax = tabela_final.plot(x='Ano', y='Média', kind='line')
-ax.locator_params(integer=True)
-plt.show()
+#print(tabela_final.head(4))
+#ax = tabela_final.plot(x='Ano', y='Média', kind='line')
+#ax.locator_params(integer=True)
+#plt.show()
 
 #Gráfico 2 - Comparativo entre a Média de gols em casa e fora anualmente
 
@@ -64,65 +65,63 @@ plt.show()
 #ax2.locator_params(integer=True)
 #plt.show()
 
-graf2 = host_subplot(111)
+#graf2 = host_subplot(111)
 
-graf2.set_xlabel("Ano")
-graf2.set_ylabel("Média de Gols por Partida")
+#graf2.set_xlabel("Ano")
+#graf2.set_ylabel("Média de Gols por Partida")
 
-p1, = graf2.plot(years, medias_gols_mandante_por_ano, color='orange', label="Média Mandante")
-p2, = graf2.plot(years, medias_gols_visitante_por_ano, color='g', label="Média Visitante")
+#p1, = graf2.plot(years, medias_gols_mandante_por_ano, color='orange', label="Média Mandante")
+#p2, = graf2.plot(years, medias_gols_visitante_por_ano, color='g', label="Média Visitante")
 
-leg = plt.legend()
+#leg = plt.legend()
 
-graf2.xaxis.get_label().set_color('r')
-graf2.xaxis.set_major_locator(MaxNLocator(integer=True))
+#graf2.xaxis.get_label().set_color('r')
+#graf2.xaxis.set_major_locator(MaxNLocator(integer=True))
 
-graf2.yaxis.get_label().set_color('c')
+#graf2.yaxis.get_label().set_color('c')
 
-leg.texts[0].set_color(p1.get_color())
-leg.texts[1].set_color(p2.get_color())
+#leg.texts[0].set_color(p1.get_color())
+#leg.texts[1].set_color(p2.get_color())
 
 #plt.show()
 
 #Gráfico 3
 
-print(qt_partidas_sem_gol)
-print(qt_partidas_com_gol)
-labels = years
-men_means = qt_partidas_sem_gol
-women_means = qt_partidas_com_gol
+#print(qt_partidas_sem_gol)
+#print(qt_partidas_com_gol)
+#labels = years
+#men_means = qt_partidas_sem_gol
+#women_means = qt_partidas_com_gol
 
-x = np.arange(len(labels))  # the label locations
-width = 0.35  # the width of the bars
+#x = np.arange(len(labels))  # the label locations
+#width = 0.35  # the width of the bars
 
-fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2, men_means, width, label='Sem Gols')
-rects2 = ax.bar(x + width/2, women_means, width, label='Com Gols')
+#fig, ax = plt.subplots()
+#rects1 = ax.bar(x - width/2, men_means, width, label='Sem Gols')
+#rects2 = ax.bar(x + width/2, women_means, width, label='Com Gols')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Partidas')
-ax.set_title('Partidas com Gols x Partida sem Gols')
-ax.set_xticks(x)
-ax.set_xticklabels(labels)
-ax.legend()
+#ax.set_ylabel('Partidas')
+#ax.set_title('Partidas com Gols x Partida sem Gols')
+#ax.set_xticks(x)
+#ax.set_xticklabels(labels)
+#ax.legend()
 
 
-def autolabel(rects):
-    """Attach a text label above each bar in *rects*, displaying its height."""
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
+# def autolabel(rects):
+#     """Attach a text label above each bar in *rects*, displaying its height."""
+#     for rect in rects:
+#         height = rect.get_height()
+#         ax.annotate('{}'.format(height),
+#                     xy=(rect.get_x() + rect.get_width() / 2, height),
+#                     xytext=(0, 3),  # 3 points vertical offset
+#                     textcoords="offset points",
+#                     ha='center', va='bottom')
 
 
-autolabel(rects1)
-autolabel(rects2)
+#autolabel(rects1)
+#autolabel(rects2)
 
-fig.tight_layout()
+#fig.tight_layout()
 
-plt.show()
-
-
+#plt.show()
