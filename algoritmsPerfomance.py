48aced4a4856aa120460107dabd9abe31a557e65
Argonauta <filcarx@gmail.com>
2019-11-14 01:38:54 -0300

    Gráficos com colunas da partida diferentes

--- algoritmsPerfomance.py
+++ algoritmsPerfomance.py
@@ -5,12 +5,13 @@ from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, Ran
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 from lib.functions import GetTrainTest, getMeanMedianAccuracyPredict
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+recursoes = 100
 
 attribute_train, attribute_test, result_train, result_test, columnsArray = GetTrainTest()
 
@@ -21,6 +22,11 @@ clfNaiveBayes = GaussianNB()
 clfAdaBoost = AdaBoostClassifier(n_estimators=200)
 
 clfGradient = GradientBoostingClassifier()
+clfGradient.fit(attribute_train, result_train)
+forecast = clfGradient.predict(attribute_test)
+
+classificationReport = classification_report(result_test, forecast)
+print(classificationReport)
 
 clfDecisionTree = DecisionTreeClassifier(
     random_state=0, max_depth=20, max_leaf_nodes=3, criterion="entropy", splitter="random")
@@ -28,19 +34,24 @@ clfDecisionTree = DecisionTreeClassifier(
 clfRandomForest = RandomForestClassifier(n_jobs=2, random_state = 0)
 
 clfSvc = SVC(kernel="rbf", degree=10)
+clfSvc.fit(attribute_train, result_train)
+forecast1 = clfSvc.predict(attribute_test)
+
+classificationReport1 = classification_report(result_test, forecast1)
+print(classificationReport1)
 
 clfMultiLayerPerceptron = MLPClassifier(
     hidden_layer_sizes=(30, 17, 9, 3), 
     solver="adam", learning_rate="adaptive", alpha=0.000015, learning_rate_init=0.001)
 
-meanDummy, medianDummy = getMeanMedianAccuracyPredict(0, 50, [], clfDummy, attribute_train, attribute_test, result_train, result_test)
-meanNaive, medianNaive = getMeanMedianAccuracyPredict(0, 50, [], clfNaiveBayes, attribute_train, attribute_test, result_train, result_test)
-meanAdaBoost, medianAdaBoost = getMeanMedianAccuracyPredict(0, 50, [], clfAdaBoost, attribute_train, attribute_test, result_train, result_test)
-meanGradient, medianGradient = getMeanMedianAccuracyPredict(0, 50, [], clfGradient, attribute_train, attribute_test, result_train, result_test)
-meanDecisionTree, medianDecisionTree = getMeanMedianAccuracyPredict(0, 50, [], clfDecisionTree, attribute_train, attribute_test, result_train, result_test)
-meanRandomForest, medianRandomForest = getMeanMedianAccuracyPredict(0, 50, [], clfRandomForest, attribute_train, attribute_test, result_train, result_test)
-meanSvc, medianSvc = getMeanMedianAccuracyPredict(0, 50, [], clfSvc, attribute_train, attribute_test, result_train, result_test)
-meanMultiLayer, medianMultiLayer = getMeanMedianAccuracyPredict(0, 50, [], clfMultiLayerPerceptron, attribute_train, attribute_test, result_train, result_test)
+meanDummy, medianDummy = getMeanMedianAccuracyPredict(0, recursoes, [], clfDummy, attribute_train, attribute_test, result_train, result_test)
+meanNaive, medianNaive = getMeanMedianAccuracyPredict(0, recursoes, [], clfNaiveBayes, attribute_train, attribute_test, result_train, result_test)
+meanAdaBoost, medianAdaBoost = getMeanMedianAccuracyPredict(0, recursoes, [], clfAdaBoost, attribute_train, attribute_test, result_train, result_test)
+meanGradient, medianGradient = getMeanMedianAccuracyPredict(0, recursoes, [], clfGradient, attribute_train, attribute_test, result_train, result_test)
+meanDecisionTree, medianDecisionTree = getMeanMedianAccuracyPredict(0, recursoes, [], clfDecisionTree, attribute_train, attribute_test, result_train, result_test)
+meanRandomForest, medianRandomForest = getMeanMedianAccuracyPredict(0, recursoes, [], clfRandomForest, attribute_train, attribute_test, result_train, result_test)
+meanSvc, medianSvc = getMeanMedianAccuracyPredict(0, recursoes, [], clfSvc, attribute_train, attribute_test, result_train, result_test)
+meanMultiLayer, medianMultiLayer = getMeanMedianAccuracyPredict(0, recursoes, [], clfMultiLayerPerceptron, attribute_train, attribute_test, result_train, result_test)
 
 means = [meanDummy, meanNaive, meanAdaBoost, meanGradient, meanDecisionTree, meanRandomForest, meanSvc, meanMultiLayer]
 medians = [medianDummy, medianNaive, medianAdaBoost, medianGradient, medianDecisionTree, medianRandomForest, medianSvc, medianMultiLayer]
@@ -48,6 +59,10 @@ algorithms = ["DummyClassifier", "Naive Bayes", "AdaBoost", "GradientBoosting", 
 
 df = pd.DataFrame({'Médias': means, 'Medianas': medians}, index=algorithms)
 
-axes = df.plot.bar(rot=0, subplots=True)
-# axes[1].legend(loc=2)
+ax1 = df["Médias"].plot.bar(rot=0, subplots=True)
+
+plt.show()
+
+ax2 = df["Medianas"].plot.bar(rot=0, subplots=True)
+
 plt.show()
